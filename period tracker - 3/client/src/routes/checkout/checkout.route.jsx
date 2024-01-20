4ef5e0d10ee71e7ca87f0ebec1e0c015980adac0
Kandagatla Lavanya Lahari <100126760+LavanyaLahari811@users.noreply.github.com>
2024-01-21 01:34:00 +0530

    checkout route

---
+++ period tracker - 3/client/src/routes/checkout/checkout.route.jsx
@@ -0,0 +1,12 @@
+import "./checkout.style.scss";
+
+const CheckOut = () => {
+    const ProductArr=[];
+  return (
+    <div className="check-out">
+      <h1>hey this is checkout</h1>
+    </div>
+  );
+};
+
+export default CheckOut;
